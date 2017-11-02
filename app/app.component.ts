a7a563de102b50b8db8c61143ad99221acb84415
LD MacKrell <ldmackrell@gmail.com>
2017-11-02 00:35:23 -0700

    Update app with additional intialization files and creation of app.component and animal-list.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal } from './animal.model';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,9 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  animals: animals[] = [
+  title = 'The World Famous CryptoZoo!'
+
+  masterTaskList: animals[] = [
     new Animal('Fiji Mermaid', 'Doug', 32, 'fish, red corals, warmwater crustacean, waterlogged sailors', 'Northend Pools', 10, 'female', 'mangoes and 80s pop', 'local politics'),
     new Animal('Naga', 'Floyd', 125, 'rodents, figs and clay bowls', 'Desert Flats', 3, 'male', 'The New York Times and flower arrangements', 'purple hats'),
     new Animal('Owl Girl', 'Kyrie', 4, 'rodents, other small birds, and the dreams of new mothers', 'Aerie', 3, 'female', 'Cirque de Soliel', 'Animaniacs and Gerbils'),
@@ -16,30 +19,14 @@ export class AppComponent {
     new Animal('Cereberus Doodle', 'Fifi', 2, 'brimstone biscuits, bone kibble, corpse ears', 'Glass Caves', 2, 'male', 'electrical frisbees and tummy rubs', 'uncats' ),
     new Animal('Uncat', 'Mr. Cat', 1, 'bone kibble', 'Dakota Plains', 4, 'male', 'Kant and dead things', 'water'),
     new Animal('N-otter', 'Arsenio', 3, 'fish, shellfish, mixed fruits and vegetables', 'Northend Pools', 6, 'male', 'toys, high tea, ascots', 'tracksuits and sideburns'),
-    new Animal('Vegetable Lamb', 'Flopsy, Mopsy, and Bertrund' 12, 'grass, clover, and lemonade', 'Dakota Plains', 1, 'hermaphroditic', 'sunsets and ear scritches', 'weedwackers'),
+    new Animal('Vegetable Lamb', 'Flopsy, Mopsy, and Bertrund', 12, 'grass, clover, and lemonade', 'Dorset Forest', 1, 'hermaphroditic', 'sunsets and ear scritches', 'weedwackers'),
     new Animal('Teacup Kraken', 'Boris', 4, 'fish, shellfish, biscuits', 'Northend Pools', 4, 'male', 'shrimp flavored sugar cubes and silent films', 'rom-coms'),
     new Animal('Closet Creeper', 'Vidocq', 20, 'pocket lint, single socks, fear', 'Suburban Sprawl', 10, 'male', 'laughter and the color pink', 'Thrash Metal and cedar'),
     new Animal('Hedgepixie', 'Thorgood', 55, 'nettles, pine cones, unsupported rumor', 'Dorset Forest', 4, 'female', 'rustling leaves, tennis balls, and midcentury jazz', 'rudeness and sloppy penmanship'),
-    new Animal('Pipe Tapper', 'Ted', 3, 'vegetable scraps and flouride-infused water', 'Suburban Sprawl', 8, 'male', ''),
-    new Animal(''),
-    new Animal(''),
-    new Animal(''),
-    new Animal(''),
-    new Animal(''),
-    new Animal(''),
-    new Animal(''),
-  ]
-
-  export class Animal {
-    species: string;
-    name: string;
-    age: number;
-    diet: string;
-    location: string;
-    caretakers: number;
-    sex: string;
-    likes: string;
-    dislikes: string
-  }
-
+    new Animal('Pipe Tapper', 'Ted', 3, 'vegetable scraps and flouride-infused water', 'Suburban Sprawl', 8, 'male', 'well-maintained plumbing and percussion instruments', 'bleach and acidic fruit'),
+    new Animal('Mongolian Death Worm', 'Brunhilda', 1000, 'camels and parasitic plants local to the Gobi desert', 'Desert Flats', 12, 'female', 'soap operas and dried figs', 'high pitched noises and junk mail' ),
+    new Animal('Macedonian Harpy', 'Ljubica', 44, 'stone fuits, fish, and small rodents', 'Aerie', 7, 'female' 'Lipsmackers lipbalm, climbing hooks, and rope swings', 'Cayenne pepper, cold temperatures, and sailors'),
+    new Animal('Macedonian Harpy', 'Snejana', 44, 'stone fuits, fish, and small rodents', 'Aerie', 7, 'female' 'Lipsmackers lipbalm, Tiger Beat magazines, and rope swings', 'Garlic salt, cold temperatures, and sailors'),
+    new Animal('Dobhar-chu (Water-hound)', 'Rex', 2, 'fish, kelp, small amphibians', 'Northend Pools', 6, 'male', 'tennis balls and fish heads', 'heat, balloons, and spam'),
+  ];
 }
---
+++ app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+import { Animal } from './animal.model';
+import { AnimalListComponent } from './animal-list.component';
+import { NewAnimalComponent } from './new-animal.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AnimalListComponent,
+    NewAnimalComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
